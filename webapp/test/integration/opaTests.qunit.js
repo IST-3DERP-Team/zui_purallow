dc9a4ed6e506cd799d524ed041760a066f816118
Michael Argulla <michael.argulla@ist-systems.com>
2024-02-08 08:05:34 +0000

    my first time

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["zuipurallow/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"zui_purallow/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
